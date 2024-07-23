73d421f082f76b6edb3f6725fe012066b151cdc9
Patrick Brennan <patrickabrennan@gmail.com>
2024-07-23 09:07:44 -0400

    Update hcpb-storage-bucket.tf

--- hcpb-storage-bucket.tf
+++ hcpb-storage-bucket.tf
@@ -3,7 +3,7 @@ resource "boundary_storage_bucket" "boundary_storage_bucket" {
   scope_id        = "global"
   plugin_name     = "aws"
   bucket_name     = aws_s3_bucket.boundary_session_recording_bucket.bucket
-  attributes_json = jsonencode({ "region" = "eu-west-2", "disable_credential_rotation" = true })
+  attributes_json = jsonencode({ "region" = "us-east-1", "disable_credential_rotation" = true })
 
   secrets_json = jsonencode({
     "access_key_id"     = var.aws_access,
