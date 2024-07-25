73cfd6b93759d2312c471962f9c9a3a911f905b0
Patrick Brennan <patrickabrennan@gmail.com>
2024-07-25 14:40:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ terraform {
     #PB added 7/25/2024
     aws = {
       source  = "hashicorp/aws"
-      version >= "4.46.0"
+      version = ">= 4.46.0"
     }
     time = {
       source  = "hashicorp/time"
