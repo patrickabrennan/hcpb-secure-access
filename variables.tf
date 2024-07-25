060c25a663d050666ae992489670d88c4c79b2b2
Patrick Brennan <patrickabrennan@gmail.com>
2024-07-24 20:23:01 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
+variable "tfc_vault_dynamic_credentials" {
+  description = "Object containing Vault dynamic credentials configuration"
+  type = object({
+    default = object({
+      token_filename = string
+      address = string
+      namespace = string
+      ca_cert_file = string
+    })
+    aliases = map(object({
+      token_filename = string
+      address = string
+      namespace = string
+      ca_cert_file = string
+    }))
+  })
+}
+
 variable "boundary_addr" {
   type = string
 }
