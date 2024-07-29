6d32c1f4d6c9ef2228c99ccbfca7ed2c591e116c
Patrick Brennan <patrickabrennan@gmail.com>
2024-07-29 12:09:11 -0400

    Update aws-ec2.tf

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -4,6 +4,24 @@ resource "aws_instance" "boundary_public_target" {
   availability_zone = var.availability_zone
   #user_data_base64  = data.cloudinit_config.ssh_trusted_ca.rendered
   user_data = <<-EOF
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
 #!/bin/bash
 sudo bash -c 'curl -o /etc/ssh/trusted-user-ca-keys.pem \
 --header "X-Vault-Namespace: admin" \
