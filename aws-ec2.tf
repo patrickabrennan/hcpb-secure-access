9aff5840ac0cc2e49eec8a552dc4bb57bb96485c
Patrick Brennan <patrickabrennan@gmail.com>
2024-07-29 17:53:39 -0400

    Update aws-ec2.tf

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -4,32 +4,33 @@ resource "aws_instance" "boundary_public_target" {
   availability_zone = var.availability_zone
   #user_data_base64  = data.cloudinit_config.ssh_trusted_ca.rendered
   user_data = <<-EOF
-#Content-Type: multipart/mixed; boundary="//"
-#MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
 
-#--//
-#Content-Type: text/cloud-config; charset="us-ascii"
-#MIME-Version: 1.0
-#Content-Transfer-Encoding: 7bit
-#Content-Disposition: attachment; filename="cloud-config.txt"
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
 
-##cloud-config
-#cloud_final_modules:
-#- [scripts-user, always]
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
 
-#--//
-#Content-Type: text/x-shellscript; charset="us-ascii"
-#MIME-Version: 1.0
-#Content-Transfer-Encoding: 7bit
-#Content-Disposition: attachment; filename="userdata.txt"
-##!/bin/bash
-#sudo bash -c 'curl -o /etc/ssh/trusted-user-ca-keys.pem \
-#--header "X-Vault-Namespace: admin" \
-#-X GET \
-#${var.vault_addr}/v1/ssh-client-signer/public_key'
-#sudo bash -c 'echo TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem >> /etc/ssh/sshd_config'
-#sudo bash -c 'systemctl restart sshd.service'
-#EOF
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+sudo bash -c 'curl -o /etc/ssh/trusted-user-ca-keys.pem \
+--header "X-Vault-Namespace: admin" \
+-X GET \
+${var.vault_addr}/v1/ssh-client-signer/public_key'
+sudo bash -c 'echo TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem >> /etc/ssh/sshd_config'
+sudo bash -c 'systemctl restart sshd.service'
+EOF
 
 
   network_interface {
@@ -50,29 +51,29 @@ resource "aws_network_interface" "boundary_public_target_ni" {
 }
 
 //Configure the EC2 host to trust Vault as the CA
-data "cloudinit_config" "ssh_trusted_ca" {
-  gzip          = false
-  base64_encode = true
+#data "cloudinit_config" "ssh_trusted_ca" {
+#  gzip          = false
+#  base64_encode = true
 
-  part {
-    content_type = "text/x-shellscript"
-    content      = <<-EOF
-    #!/bin/bash
-    sudo bash -c 'curl -o /etc/ssh/trusted-user-ca-keys.pem \
-    --header "X-Vault-Namespace: admin" \
-    -X GET \
-    ${var.vault_addr}/v1/ssh-client-signer/public_key'
-    sudo bash -c 'echo TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem >> /etc/ssh/sshd_config'
-    sudo bash -c 'systemctl restart sshd.service'
-    EOF
-  }
+#  part {
+#    content_type = "text/x-shellscript"
+#    content      = <<-EOF
+#    #!/bin/bash
+#    sudo bash -c 'curl -o /etc/ssh/trusted-user-ca-keys.pem \
+#    --header "X-Vault-Namespace: admin" \
+#    -X GET \
+#    ${var.vault_addr}/v1/ssh-client-signer/public_key'
+#    sudo bash -c 'echo TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem >> /etc/ssh/sshd_config'
+#    sudo bash -c 'systemctl restart sshd.service'
+#    EOF
+#  }
 
-  part {
-    content_type = "text/x-shellscript"
-    content      = <<-EOF
-    #!/bin/bash
-    sudo bash -c 'adduser admin_user'
-    sudo nash -c 'adduser danny'
-    EOF
-  }
-}
+#  part {
+#    content_type = "text/x-shellscript"
+#    content      = <<-EOF
+#    #!/bin/bash
+#    sudo bash -c 'adduser admin_user'
+#    sudo nash -c 'adduser danny'
+#    EOF
+#  }
+#}
