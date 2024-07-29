9ffb3254a975a0db346d47d893adc9cb1a1fd9a5
Patrick Brennan <patrickabrennan@gmail.com>
2024-07-29 09:00:35 -0400

    Update aws-ec2.tf

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -2,7 +2,15 @@ resource "aws_instance" "boundary_public_target" {
   ami               = var.aws_ami
   instance_type     = "t2.micro"
   availability_zone = var.availability_zone
-  user_data_base64  = data.cloudinit_config.ssh_trusted_ca.rendered
+  #user_data_base64  = data.cloudinit_config.ssh_trusted_ca.rendered
+  user_data = <<-EOF
+    sudo bash -c 'curl -o /etc/ssh/trusted-user-ca-keys.pem \
+    --header "X-Vault-Namespace: admin" \
+    -X GET \
+    ${var.vault_addr}/v1/ssh-client-signer/public_key'
+    sudo bash -c 'echo TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem >> /etc/ssh/sshd_config'
+    sudo bash -c 'systemctl restart sshd.service'
+    EOF
 
   network_interface {
     network_interface_id = aws_network_interface.boundary_public_target_ni.id
@@ -22,27 +30,27 @@ resource "aws_network_interface" "boundary_public_target_ni" {
 }
 
 //Configure the EC2 host to trust Vault as the CA
-data "cloudinit_config" "ssh_trusted_ca" {
-  gzip          = false
-  base64_encode = true
+#data "cloudinit_config" "ssh_trusted_ca" {
+#  gzip          = false
+#  base64_encode = true
 
-  part {
-    content_type = "text/x-shellscript"
-    content      = <<-EOF
-    sudo bash -c 'curl -o /etc/ssh/trusted-user-ca-keys.pem \
-    --header "X-Vault-Namespace: admin" \
-    -X GET \
-    ${var.vault_addr}/v1/ssh-client-signer/public_key'
-    sudo bash -c 'echo TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem >> /etc/ssh/sshd_config'
-    sudo bash -c 'systemctl restart sshd.service'
-    EOF
-  }
+#  part {
+#    content_type = "text/x-shellscript"
+#    content      = <<-EOF
+#    sudo bash -c 'curl -o /etc/ssh/trusted-user-ca-keys.pem \
+#    --header "X-Vault-Namespace: admin" \
+#    -X GET \
+#    ${var.vault_addr}/v1/ssh-client-signer/public_key'
+#    sudo bash -c 'echo TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem >> /etc/ssh/sshd_config'
+#    sudo bash -c 'systemctl restart sshd.service'
+#    EOF
+#  }
 
-  part {
-    content_type = "text/x-shellscript"
-    content      = <<-EOF
-    sudo bash -c 'adduser admin_user'
-    sudo nash -c 'adduser danny'
-    EOF
-  }
-}
+#  part {
+#    content_type = "text/x-shellscript"
+#    content      = <<-EOF
+#    sudo bash -c 'adduser admin_user'
+#    sudo nash -c 'adduser danny'
+#    EOF
+#  }
+#}
