44411cf102a410fec041fc49973021e6fe21564c
Patrick Brennan <patrickabrennan@gmail.com>
2024-08-01 13:31:50 -0400

    Update credential-library.tf

--- credential-library.tf
+++ credential-library.tf
@@ -12,7 +12,8 @@ resource "vault_token" "boundary_vault_token" {
 resource "boundary_credential_store_vault" "vault_cred_store" {
   name        = "boundary-vault-credential-store"
   description = "Vault Credential Store"
-  address     = var.vault_addr
+  #PB 8/1/2024 took address out as using Vault / TF Dynamci Creds
+  #address     = var.vault_addr
   token       = vault_token.boundary_vault_token.client_token
   namespace   = "admin"
   scope_id    = boundary_scope.project.id
