b372e291d3aff546d043538c1e8dc0554779dd9a
Patrick Brennan <patrickabrennan@gmail.com>
2024-08-01 14:01:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,26 +53,27 @@ provider "aws" {
 
 provider "vault" {
   // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
-  ##skip_child_token = true
-  ##address          = var.tfc_vault_dynamic_credentials.default.address
-  ##namespace        = var.tfc_vault_dynamic_credentials.default.namespace
-  ##token = var.vault_token
-  #auth_login_token_file {
-  #  filename = var.tfc_vault_dynamic_credentials.default.token_filename
-  #}
+  skip_child_token = true
+  address          = var.tfc_vault_dynamic_credentials.default.address
+  namespace        = var.tfc_vault_dynamic_credentials.default.namespace
+
+  auth_login_token_file {
+    filename = var.tfc_vault_dynamic_credentials.default.token_filename
+  }
 }
 
-#provider "vault" {
-#  // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
-#  skip_child_token = true
-#  alias            = "ALIAS1"
-#  address          = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].address
-#  namespace        = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].namespace
+provider "vault" {
+  // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
+  skip_child_token = true
+  alias            = "ALIAS1"
+  address          = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].address
+  namespace        = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].namespace
 
-#  auth_login_token_file {
-#    filename = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].token_filename
-#  }
-#}
+  auth_login_token_file {
+    filename = var.tfc_vault_dynamic_credentials.aliases["ALIAS1"].token_filename
+  }
+}
+
 
 
 
