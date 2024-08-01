1bf58ad6c0d03310b48a48cf51286ac6308e8082
Patrick Brennan <patrickabrennan@gmail.com>
2024-08-01 12:41:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,10 +53,10 @@ provider "aws" {
 
 provider "vault" {
   // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
-  skip_child_token = true
-  address          = var.tfc_vault_dynamic_credentials.default.address
-  namespace        = var.tfc_vault_dynamic_credentials.default.namespace
-  token = var.vault_token
+  ##skip_child_token = true
+  ##address          = var.tfc_vault_dynamic_credentials.default.address
+  ##namespace        = var.tfc_vault_dynamic_credentials.default.namespace
+  ##token = var.vault_token
   #auth_login_token_file {
   #  filename = var.tfc_vault_dynamic_credentials.default.token_filename
   #}
