2ff9c653b9d8b8c195d2edc9b3c24a46a7b4f0e8
Patrick Brennan <patrickabrennan@gmail.com>
2025-09-28 17:51:17 -0400

    Update windows.tf

--- windows.tf
+++ windows.tf
@@ -22,7 +22,7 @@ data "aws_ami" "windows" {
 resource "aws_instance" "rdp_target" {
   ami                         = data.aws_ami.windows.id
   instance_type               = "t3.small"
-  key_name                    = "testing"                 # you already have this
+  key_name                    = "sap"                 # you already have this
   subnet_id                   = aws_subnet.boundary_db_demo_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_all.id]
   monitoring                  = true
