f7affae956d409ca5a9d6f57a838f6193ec19015
Patrick Brennan <patrickabrennan@gmail.com>
2025-09-28 11:52:07 -0400

    Update windows.tf

--- windows.tf
+++ windows.tf
@@ -28,7 +28,8 @@ resource "aws_instance" "rdp-target" {
   monitoring             = true
   subnet_id              = aws_subnet.boundary_db_demo_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_all.id]
-  user_data              = templatefile("./template_files/windows-target.tftpl", { admin_pass = var.rdp_admin_pass })
+  #commnet ou 9-28-2025
+  #user_data              = templatefile("./template_files/windows-target.tftpl", { admin_pass = var.rdp_admin_pass })
   tags = {
     Team = "IT"
     Name = "rdp-target"
