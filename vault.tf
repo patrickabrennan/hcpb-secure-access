dfce408e28ed4b6530d6092bae8dc473c52440fe
Patrick Brennan <patrickabrennan@gmail.com>
2026-01-26 18:22:55 -0500

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,3 +1,25 @@
+# If you don't already have a KV v1 mount at "kv", set create_kv_mount=true and include this block.
+
+resource "vault_mount" "kv" {
+  count       = var.create_kv_mount ? 1 : 0
+  path        = var.vault_kv_mount_path
+  type        = "kv" # KV v1 (flat data) for Boundary RDP credentials
+  description = "KV v1 for Boundary RDP credentials"
+}
+
+# Write a flat secret (username/password) so Boundary can map it to credential_type = "username_password"
+resource "vault_generic_secret" "rdp_admin" {
+  path = "${var.vault_kv_mount_path}/${var.vault_kv_secret_path}" # e.g., "kv/boundary/rdp/svc"
+
+  data_json = jsonencode({
+    username = "Administrator"
+    password = local.admin_password
+  })
+}
+
+
+
+/*
 # If you don't already have a KV v2 mount at "kv", set create_kv_mount=true and include this block.
 
 resource "vault_mount" "kv" {
@@ -16,3 +38,4 @@ resource "vault_kv_secret_v2" "rdp_admin" {
     password = local.admin_password
   })
 }
+*/
