d12e3573ff0b0ee2691ae317b7d17fc49635d31e
Patrick Brennan <patrickabrennan@gmail.com>
2026-01-26 18:24:14 -0500

    Update vault-policy.tf

--- vault-policy.tf
+++ vault-policy.tf
@@ -37,6 +37,97 @@ path "kv/*" {
 EOT
 }
 
+resource "vault_policy" "policy-database" {
+  name   = "policy-database"
+  policy = <<EOT
+path "database/creds/dba" {
+  capabilities = ["read"]
+}
+EOT
+}
+
+//This isn't required for injected credentials, but would be required for SSH brokered
+resource "vault_policy" "ssh-policy" {
+  name   = "ssh-policy"
+  policy = <<EOT
+path "ssh-client-signer/sign/boundary-client" {
+  capabilities = ["create","read","update","delete","list","sudo"]
+}
+EOT
+}
+
+#added 9-25-2025
+# vault-policy.tf
+resource "vault_policy" "policy_windows_rdp" {
+  name   = "policy-windows-rdp"
+  policy = <<-EOT
+  path "kv/boundary/rdp/svc" {
+    capabilities = ["read"]
+  }
+  EOT
+}
+#end add 9-25-2025
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+resource "vault_policy" "boundary_controller_policy" {
+  name   = "boundary-controller"
+  policy = <<EOT
+path "auth/token/lookup-self" {
+  capabilities = ["read"]
+}
+
+path "auth/token/renew-self" {
+  capabilities = ["update"]
+}
+
+path "auth/token/revoke-self" {
+  capabilities = ["update"]
+}
+
+path "sys/leases/renew" {
+  capabilities = ["update"]
+}
+
+path "sys/leases/revoke" {
+  capabilities = ["update"]
+}
+
+path "sys/capabilities-self" {
+  capabilities = ["update"]
+}
+EOT
+}
+
+//This isn't required for injected credentials, but would be required for SSH brokered
+resource "vault_policy" "kv-policy" {
+  name   = "kv-read"
+  policy = <<EOT
+path "kv/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+EOT
+}
+
 resource "vault_policy" "ssh-policy" {
   name   = "ssh-policy"
   policy = <<EOT
@@ -70,4 +161,4 @@ resource "vault_policy" "policy_windows_rdp" {
   EOT
 }
 #end add 9-25-2025
-
+*/
