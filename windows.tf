c124c8673d780d79da98400d9e1fdb5eb11806b7
Patrick Brennan <patrickabrennan@gmail.com>
2026-01-26 16:24:16 -0500

    Update windows.tf

--- windows.tf
+++ windows.tf
@@ -20,75 +20,6 @@ data "aws_ami" "windows" {
 }
 
 resource "aws_instance" "rdp_target" {
-  ami                         = var.aws_ami != "" ? var.aws_ami : data.aws_ami.windows.id
-  instance_type               = var.aws_instance_type
-  subnet_id                   = aws_subnet.boundary_db_demo_subnet.id
-  vpc_security_group_ids      = [aws_security_group.allow_all.id]
-  associate_public_ip_address = true
-  key_name                    = var.admin_key_name != "" ? var.admin_key_name : null
-
-  user_data = <<-POW
-  <powershell>
-  Set-ItemProperty "HKLM:\\System\\CurrentControlSet\\Control\\Terminal Server" -Name "fDenyTSConnections" -Value 0
-  Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-  </powershell>
-  POW
-
-  tags = { Team = "IT", Name = "rdp-target" }
-}
-
-locals {
-  # NOTE: resource name must match your EC2 resource in windows.tf
-  # Decrypt EC2 Windows password_data only when a private key PEM is provided.
-  # Otherwise fall back to a provided plaintext password (decrypted_admin_password), or empty string.
-  admin_password = try(
-    (var.admin_key_private_pem != "" ? rsadecrypt(aws_instance.rdp_target.password_data, var.admin_key_private_pem) : null),
-    var.decrypted_admin_password,
-    ""
-  )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-# Windows Target
-data "aws_ami" "windows" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["Windows_Server-2022-English-Full-Base-*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-resource "aws_instance" "rdp_target" {
   ami                         = data.aws_ami.windows.id
   instance_type               = "t3.small"
   key_name                    = "sap"                 # you already have this
@@ -116,4 +47,3 @@ locals {
     ""
   )
 }
-*/
