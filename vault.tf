a717eef6d6ae2b7af623f2e03183bfcb68861c43
Patrick Brennan <patrickabrennan@gmail.com>
2026-01-26 20:06:26 -0500

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,3 +1,34 @@
+resource "vault_mount" "kv_rdp" {
+  count       = var.create_kv_mount ? 1 : 0
+  path        = var.vault_kv_mount_path   # "kv-rdp"
+  type        = "kv"                      # KV v1
+  description = "KV v1 for Boundary RDP credentials (flat username/password)"
+}
+
+resource "vault_generic_secret" "rdp_admin" {
+  path = "${var.vault_kv_mount_path}/${var.vault_kv_secret_path}" # kv-rdp/boundary/rdp/svc
+
+  data_json = jsonencode({
+    username = "Administrator"
+    password = local.admin_password
+  })
+
+  depends_on = [vault_mount.kv_rdp]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 # Create a dedicated KV v1 mount just for Boundary RDP injection.
 # This avoids fighting with an existing "kv" mount that is likely KV v2 in HCP Vault.
 
@@ -17,3 +48,4 @@ resource "vault_generic_secret" "rdp_admin" {
     password = local.admin_password
   })
 }
+*/
